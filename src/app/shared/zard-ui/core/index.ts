806d7fdabde52ac3ba295fd567c0457efae8f4e1
Raffael Bino <raffael_bino18@yahoo.com.br>
2026-02-08 18:18:56 -0300

    feat(): Atualizar componente Card do ZardUI

---
+++ src/app/shared/zard-ui/core/index.ts
@@ -0,0 +1,4 @@
+export * from './directives/string-template-outlet/string-template-outlet.directive';
+export * from './directives/id.directive';
+export * from './provider/providezard';
+export * from './provider/event-manager-plugins/zard-event-manager-plugin';
